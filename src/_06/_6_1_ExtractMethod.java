21c198def03e82a16017fc81357403f4e300a2d6
lujiango <lujiang200829@126.com>
2016-09-04 12:11:53 +0800

    add extract method pattern

---
+++ src/_06/_6_1_ExtractMethod.java
@@ -0,0 +1,38 @@
+package _06;
+
+/**
+ * 有一段代码可以被组织在一起并独立出来<br>
+ * 将一段代码放进一个独立函数中，并让函数名称解释该函数的用途。<br>
+ * 好处：
+ * 函数粒度小，复用机会大，覆写也容易，可读性增强。
+ * 
+ * @author lujiang
+ *
+ */
+public class _6_1_ExtractMethod {
+	private String name;
+
+	void printBanner() {
+		System.out.println("printBanner");
+	}
+
+	// printOwing函数实现
+	void printOwing(double amount) {
+		printBanner();
+		System.out.println("name : " + name);
+		System.out.println("amount : " + amount);
+	}
+
+	// printOwingNew为合理实现
+	void printOwingNew(double amount) {
+		printBanner();
+		printDetails(amount);
+	}
+
+	// 提炼的方法printDetails
+	void printDetails(double amount) {
+		System.out.println("name : " + name);
+		System.out.println("amount : " + amount);
+	}
+
+}
